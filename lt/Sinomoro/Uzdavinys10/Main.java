4bb9d05179ea72597b0018e740239a218fe2568b
15R_Dell <Sikorskas.Aurimas@gmail.com>
2017-11-22 20:02:10 +0200

    Atliktas 10 uzdavinys

--- lt/Sinomoro/Uzdavinys10/Main.java
+++ lt/Sinomoro/Uzdavinys10/Main.java
@@ -1,9 +1,32 @@
 package lt.Sinomoro.Uzdavinys10;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
+
+
+    private static void removePVM(int kiekis, double sumaSuPVM)
+    {
+        removePVM(kiekis,sumaSuPVM,2);
+    }
+
+    private static void removePVM(int kiekis, double sumaSuPVM, int prekesKainosTikslumasbePVM)
+    {
+        BigDecimal prekesKaina = BigDecimal.valueOf(sumaSuPVM).divide(BigDecimal.valueOf(kiekis),RoundingMode.HALF_EVEN).multiply(BigDecimal.valueOf(100.0/121.0));// = (Suma/kiekio)*(100/121);
+        BigDecimal sumaBePVM = prekesKaina.multiply(BigDecimal.valueOf(kiekis));// = prekesKainaBePVM*kiekio
+
+        System.out.println(" Prekės kaina be PVM : " + prekesKaina.setScale(prekesKainosTikslumasbePVM, RoundingMode.HALF_EVEN));
+        System.out.println(" Kiekis              : " + kiekis);
+        System.out.println(" Suma be PVM         : " + sumaBePVM.setScale(2,RoundingMode.HALF_EVEN));
+        System.out.println(" PVM Suma            : " + BigDecimal.valueOf(sumaSuPVM).subtract(sumaBePVM).setScale(2,RoundingMode.HALF_EVEN));// = suma-sumaBePVM
+        System.out.println(" Viso Suma su PVM    : " + BigDecimal.valueOf(sumaSuPVM).setScale(2,RoundingMode.HALF_EVEN));
+
+    }
 
     public static void main(String[] args)
     {
-
+        Main.removePVM(10,200);
+        Main.removePVM(9,11.59);
     }
 }
